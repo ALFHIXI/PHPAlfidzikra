ad0a44fa5e031063bdfa313d0b1d5e7eb32adea3
Alfidzikra <118788594+ALFHIXI@users.noreply.github.com>
2024-04-02 22:30:36 +0700

    Delete tugas5 directory

--- tugas5/reset.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start(); // Mulai sesi
-session_unset(); // Hapus semua data sesi
-session_destroy(); // Hapus sesi
-header("Location: objMahasiswa.php"); // Redirect kembali ke halaman utama
-exit();
-?>
